fbaa79a79e8c0b13050a18b4b7bf1fdc12974c17
nazaroveg <nazaroveg@yandex.ru>
2022-08-10 12:03:53 +0300

    v1

---
+++ name_main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	std::string name;
+	std::cout << "Введите имя " << std::endl;
+	std::cin >> name;
+	std::cout << "Здравствуйте, " << name << "!" << std::endl;
+
+
+
+	return 0;
+}
